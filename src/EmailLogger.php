aa55247e5dfa153435d4715afe42213000464baf
Chris Reid <chris.reid@nexuspoint.co.uk>
2019-09-04 20:45:27 +0100

    Allow null notifiable

--- src/EmailLogger.php
+++ src/EmailLogger.php
@@ -16,7 +16,7 @@ class EmailLogger
     public function handle(MessageSent $event)
     {
         $message = $event->message;
-        $notifiable = $event->data['notifiable'];
+        $notifiable = $event->data['notifiable'] ?? null;
 
         DB::table('email_log')->insert([
             'date' => date('Y-m-d H:i:s'),
@@ -27,8 +27,8 @@ class EmailLogger
             'subject' => $message->getSubject(),
             'body' => $message->getBody(),
             'headers' => json_encode(array_filter(explode("\r\n", $message->getHeaders()->toString()))),
-            'mailable_id' => $notifiable->getKey(),
-            'mailable_type' => get_class($notifiable),
+            'mailable_id' => $notifiable ? $notifiable->getKey() : null,
+            'mailable_type' => $notifiable ? get_class($notifiable) : null,
         ]);
     }
 }
